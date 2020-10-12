37d5f3e182aff0f1e33c404ab501c86cbda632e9
sepseel <sepsel64@gmail.com>
2020-10-12 20:34:47 +0200

    component for displaying a board on the users page

---
+++ src/components/SingleBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="ma-2">
+    <v-img height="200px" :src="board.backgroundUrl"></v-img>
+    <v-card-title primary-title>
+      <div class="headline">{{board.name}}</div>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        :to="{ name: 'board', params: { id: board._id } }"
+      >Go</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['board'],
+};
+</script>
