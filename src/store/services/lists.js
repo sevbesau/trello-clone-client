841571b8ee3ce3384c5f4613985e40f8741b8816
sepseel <sepsel64@gmail.com>
2020-10-12 20:33:49 +0200

    create a service for lists

---
+++ src/store/services/lists.js
@@ -0,0 +1,53 @@
+import feathersClient, { makeServicePlugin, BaseModel } from '../../feathers-client';
+
+class List extends BaseModel {
+  static modelName = 'List';
+
+  static instanceDefaults() {
+    return {
+      name: '',
+      order: 0,
+      boardId: '',
+      archived: false,
+    };
+  }
+}
+const servicePath = 'lists';
+const servicePlugin = makeServicePlugin({
+  Model: List,
+  service: feathersClient.service(servicePath),
+  servicePath,
+});
+
+// Setup the client-side Feathers hooks.
+feathersClient.service(servicePath).hooks({
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+});
+
+export default servicePlugin;
