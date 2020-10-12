d4afc5178cc2d0cc35dc604fd6237c2e2e55be2c
sepseel <sepsel64@gmail.com>
2020-10-12 20:23:28 +0200

    install and configure feathers and feathers-vuex

---
+++ src/feathers-client.js
@@ -0,0 +1,37 @@
+import feathers from '@feathersjs/feathers';
+import socketio from '@feathersjs/socketio-client';
+import auth from '@feathersjs/authentication-client';
+import io from 'socket.io-client';
+import feathersVuex from 'feathers-vuex';
+
+const socket = io('http://localhost:3030', { transports: ['websocket'] });
+
+const feathersClient = feathers()
+  .configure(socketio(socket))
+  .configure(auth({ storage: window.localStorage }));
+
+export default feathersClient;
+
+// Setting up feathers-vuex
+const {
+  makeServicePlugin,
+  makeAuthPlugin,
+  BaseModel,
+  models,
+  FeathersVuex,
+} = feathersVuex(
+  feathersClient,
+  {
+    serverAlias: 'api', // optional for working with multiple APIs (this is the default value)
+    idField: '_id', // Must match the id field in your database table/collection
+    whitelist: ['$regex', '$options'],
+  },
+);
+
+export {
+  makeAuthPlugin,
+  makeServicePlugin,
+  BaseModel,
+  models,
+  FeathersVuex,
+};
