534ac72f88360df13a20268d95f872b05bc1a4e7
sepseel <sepsel64@gmail.com>
2020-10-12 20:30:07 +0200

    implement authentication routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,58 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Login from '@/views/Login.vue';
+import Boards from '../views/Boards.vue';
 import Home from '../views/Home.vue';
+import SignUp from '../views/SignUp.vue';
+import Board from '../views/Board.vue';
+
+import store from '../store';
 
 Vue.use(VueRouter);
+
+function isLoggedIn(to, from, next) {
+  store.dispatch('auth/authenticate')
+    .then(() => {
+      next();
+    }).catch(() => {
+      next('/login');
+    });
+}
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
+    beforeEnter(to, from, next) {
+      store.dispatch('auth/authenticate').then(() => {
+        next('/boards');
+      }).catch(() => {
+        next('/login');
+      });
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/signup',
+    name: 'signup',
+    component: SignUp,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/boards',
+    name: 'boards',
+    component: Boards,
+    beforeEnter: isLoggedIn,
+  },
+  {
+    path: '/board/:id',
+    name: 'board',
+    component: Board,
+    beforeEnter: isLoggedIn,
   },
 ];
 
